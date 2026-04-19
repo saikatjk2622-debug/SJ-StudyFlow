fd26f76a59f95b7abf6012aae3e247f62e35fd0e
SJ-StudyFlow <saikatjk2622@gmail.com>
2026-04-20 01:54:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '../SJ_StudyFlow';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(React.createElement(App));
